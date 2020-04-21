9149bc357c9585364c74372b5292230548a3a6f0
kesav1234 <50697878+kesav1234@users.noreply.github.com>
2020-04-21 17:13:48 +0530

    Add files via upload

---
+++ __mocks__/api.js
@@ -0,0 +1,5 @@
+export default class {
+    static async get() { return { data: 'mock' }; }
+    static async post() { return { data: 'mock' }; }
+    static async put() { return { data: 'mock' }; }
+}
